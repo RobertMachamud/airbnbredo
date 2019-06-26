d0a361dc3c3343fc3fc6a924d5e649e3d314bc3a
RobertMachamud <blaze1@gmx.net>
2019-06-26 16:47:18 +0700

    added query - site displays properties (exc. img)

--- controller/properties_get.js
+++ controller/properties_get.js
@@ -4,7 +4,21 @@ const db = require('../db.js')  //.js is not needed
 // export this file
 module.exports = (req, res) => {
   // select everything from properties using query
-  db.query(`SELECT * FROM properties`, (err, result) => {
+  db.query(`SELECT
+properties.id,
+properties.name,
+properties.price,
+properties.rating,
+properties.city,
+properties.country,
+countries.name AS "country",
+cities.name AS "city",
+types.name AS "type"
+
+FROM properties
+LEFT JOIN countries ON properties.country = countries.id
+LEFT JOIN cities ON properties.city = cities.id
+LEFT JOIN types ON properties.type = types.id`, (err, result) => {
     // if err comes up - display
     if (err) {
       console.log('err', err)
