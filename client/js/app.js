24c856b90773ea22fb4079a7296188ea8335aade
RobertMachamud <blaze1@gmx.net>
2019-06-27 16:05:36 +0700

    started filter - api - clicks next

--- client/js/app.js
+++ client/js/app.js
@@ -1,15 +1,24 @@
 // function that runs everything in there when side is loaded
 window.onload = () => {
-  //
+  // getting the api of properties and takes data (obj.)
   axios.get('api/properties').then( (res) => {
-    // putting data we get on 'api/prop.' into var properties
+    // putting data we get on 'api/properties' into var properties
     let properties = res.data
-    // selecting parent(container) from your properties into var props
+    // selecting parent(container) from your properties into var properties_bx
     let properties_bx = document.getElementById('prop_cont')
-    // forEach loop on the data and insert it into properties-boxes
-    properties.forEach( (p) => {
-      properties_bx.insertAdjacentHTML('beforeEnd', `
 
+
+
+    // forEach loop on the data and insert it into properties-boxes -
+    //   you get as many boxes as ther are data in your table
+    properties.forEach( (p) => {
+
+      let stars =''
+      for (i = 0; i < p.rating; i++) {
+        stars += `<i class="fas fa-star"></i>`
+      }
+
+      properties_bx.insertAdjacentHTML('beforeEnd', `
       <div class="prop_box">
         <div class="prop_img" background=""></div>
         <div class="prop_content">
@@ -20,12 +29,8 @@ window.onload = () => {
               ${p.name}
             </div>
             <div class="price">$${p.price}/night</div>
-            <div class="rating">
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
+            <div id="rating" class="rating">
+              ${stars}
             </div>
         </div>
       </div>
@@ -41,3 +46,9 @@ window.onload = () => {
 
 
 }
+
+
+// let stars =''
+// for (i = 0; i <= p.rating; i++) {
+//   stars += `<i class="fas fa-star"></i>`
+// }
--- controller/properties_get.js
+++ controller/properties_get.js
@@ -1,10 +1,20 @@
+// needed zb. to display all "properties" in the DOM
+// gets importet by index.js as api -
+//  also in app.js where you need it to not getting just numbers (id) on zb. countries/cities/... -
+
 // import database
 const db = require('../db.js')  //.js is not needed
 
-// export this file
+// export this file - function
 module.exports = (req, res) => {
-  // select everything from properties using query
-  db.query(`SELECT
+  // first: select everything from properties using query
+  // second: using query to select all items in main table like:
+  //         properties.id/name/price/...
+  //         then - bring in other tables: countries.name AS "country" -> becomes new column in properties
+  //   then - FROM properties
+  //        LEFT JOIN countries ON properties.country = countries.id   (with all of them)
+  //   now - all "properties" will be displayed in the DOM
+  let query = `SELECT
 properties.id,
 properties.name,
 properties.price,
@@ -18,13 +28,45 @@ types.name AS "type"
 FROM properties
 LEFT JOIN countries ON properties.country = countries.id
 LEFT JOIN cities ON properties.city = cities.id
-LEFT JOIN types ON properties.type = types.id`, (err, result) => {
+LEFT JOIN types ON properties.type = types.id `
+
+  if (req.query && Object.keys(req.query).length) {
+    query += `WHERE `
+  }
+
+  for (i = 0; i < Object.keys(req.query).length; i++) {
+
+    if (Object.keys(req.query)[i] == 'rating' && i == 0) {
+      query += `properties.rating = ${req.query.rating}`
+    } else if (Object.keys(req.query)[i] == 'rating') {
+      query += ` and properties.rating = ${req.query.rating}`
+    }
+
+    if (Object.keys(req.query)[i] == 'city' && i == 0) {
+      query += `properties.city = ${req.query.city}`
+    } else if (Object.keys(req.query)[i] == 'city') {
+      query += ` and properties.city = ${req.query.city}`
+    }
+
+    if (Object.keys(req.query)[i] == 'country' && i ==0) {
+      query += `properties.country = ${req.query.country}`
+    } else if (Object.keys(req.query)[i] == 'country') {
+      query += ` and properties.country = ${req.query.country}`
+    }
+  }
+
+
+
+  console.log(query);
+
+  db.query(query, (err, result) => {
     // if err comes up - display
     if (err) {
       console.log('err', err)
     } else {
-      // else - send the result in rows
+      // else - send the result in rows ()
       res.send(result.rows)
     }
   })
+
 }
